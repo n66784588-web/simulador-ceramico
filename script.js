ec784ef5dc39e6c042e13266da8aa2d5e4b9164e
n66784588-web <n66784588@gmail.com>
2026-04-18 17:37:28 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+console.log("JS NUEVO CARGADO");
 
+// =============================
+let modo = "piso";
+let texturaActual = null;
+
+// =============================
+const misProductos = [
+    { nombre: "acatlan-30x60", marca: "nitropiso" },
+    { nombre: "alameda-60x60", marca: "nitropiso" }
+];
+
+// Generar ruta automática
+misProductos.forEach(p => {
+    p.img = `img/productos/${p.nombre}.jpg`;
+});
+
+// =============================
+function mostrarProductos() {
+    const contenedor = document.getElementById("catalog-container");
+    contenedor.innerHTML = "";
+
+    misProductos.forEach(p => {
+        const div = document.createElement("div");
+
+        div.innerHTML = `
+            <img src="${p.img}" width="100">
+            <p>${p.nombre}</p>
+        `;
+
+        contenedor.appendChild(div);
+    });
+}
+
+// =============================
+window.onload = () => {
+    mostrarProductos();
+};
